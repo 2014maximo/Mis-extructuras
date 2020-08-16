ea75881f9a832a30323ac2fe289378edc380e433
2014maximo <alexuriel.777@gmail.com>
2020-08-16 13:31:09 -0500

    adaptando tabs

--- blogs/formato_post/styles/scripts.js
+++ blogs/formato_post/styles/scripts.js
@@ -10,6 +10,8 @@ function validarTab( bloque ) {
         referenciaDos.style.display = 'none';
         let referenciaTabUno = document.getElementById('html-content');
         referenciaTabUno.style.display = 'none';
+        let referenciaTabUno = document.getElementById('css-content');
+        referenciaTabUno.style.display = 'block';
         let referenciaTres = document.getElementById('css-1');
         referenciaTres.style.display = 'block';
         let referenciaCuatro = document.getElementById('html-2');
@@ -25,6 +27,10 @@ function validarTab( bloque ) {
         referenciaTres.style.display = 'none';
         let referenciaCuatro = document.getElementById('css-2');
         referenciaCuatro.style.display = 'block';
+        let referenciaTabUno = document.getElementById('css-content');
+        referenciaTabUno.style.display = 'none';
+        let referenciaTabUno = document.getElementById('html-content');
+        referenciaTabUno.style.display = 'block';
 
     } else if ( ( this.global === 'html' ) && ( ( bloque === 'js-3'))) {
         this.global = 'js';
@@ -36,6 +42,10 @@ function validarTab( bloque ) {
         referenciaTres.style.display = 'block';
         let referenciaCuatro = document.getElementById('js-1');
         referenciaCuatro.style.display = 'block';
+        let referenciaTabUno = document.getElementById('html-content');
+        referenciaTabUno.style.display = 'none';
+        let referenciaTabUno = document.getElementById('js-content');
+        referenciaTabUno.style.display = 'block';
 
     } else if ( ( this.global === 'css' ) && ( bloque === 'js-3' ) ) {
         this.global = 'js';
@@ -47,6 +57,10 @@ function validarTab( bloque ) {
         referenciaTres.style.display = 'block';
         let referenciaCuatro = document.getElementById('js-1');
         referenciaCuatro.style.display = 'block';
+        let referenciaTabUno = document.getElementById('css-content');
+        referenciaTabUno.style.display = 'none';
+        let referenciaTabUno = document.getElementById('js-content');
+        referenciaTabUno.style.display = 'block';
 
     } else if( ( this.global === 'js' ) && ( bloque === 'html-2' ) ){
         this.global = 'html';
@@ -136,5 +150,13 @@ function validarTab( bloque ) {
         let referenciaCuatro = document.getElementById('html-3');
         referenciaCuatro.style.display = 'block';
 
+    }
+
+    function destinar(varGlobal, referencia, nivel) {
+        
+        this.global = varGlobal;
+        let referencia = document.getElementById(varGlobal+'-1');
+        referencia.style.display = 'none';
+        
     }
 }
